8ff6711611be0d874bddc0ff00db31f4fa5a9bc7
Malaikamadi <madimaliaka@gmail.com>
2025-07-07 06:57:18 +0000

    Delete services directory

--- services/hasura/actions/install-deps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Install the necessary TypeScript type dependencies
-npm install --save-dev @types/node @types/express @types/cors @types/body-parser
-npm install --save zod graphql-request
